6fda1d1bf39712a61cc57af3aa4f4894ad54e218
Windroid Xie <windroid.xie@outlook.com>
2020-01-21 00:02:03 +0800

    完成怎删改查主模块的初步实现

--- commonModule.py
+++ commonModule.py
@@ -1,7 +1,17 @@
 from aliyunsdkcore.client import AcsClient
 from aliyunsdkcore.acs_exception.exceptions import ClientException
 from aliyunsdkcore.acs_exception.exceptions import ServerException
+
+from aliyunsdkalidns.request.v20150109.DescribeDomainRecordsRequest import DescribeDomainRecordsRequest
+from aliyunsdkalidns.request.v20150109.AddDomainRecordRequest import AddDomainRecordRequest
+from aliyunsdkalidns.request.v20150109.DeleteDomainRecordRequest import DeleteDomainRecordRequest
+from aliyunsdkalidns.request.v20150109.DeleteSubDomainRecordsRequest import DeleteSubDomainRecordsRequest
+from aliyunsdkalidns.request.v20150109.UpdateDomainRecordRequest import UpdateDomainRecordRequest
+
 import json
+import os
+import sys
+import urllib.request
 
 #从指定的配置文件读取用户配置
 def ReadUserConfig(configPath):
@@ -9,7 +19,7 @@ def ReadUserConfig(configPath):
         config = json.load(jsonFile)
     return config
 
-#执行request并返回结果
+#从已生成的client执行request并返回结果
 def ExecuteGetResults(client,request):
     response = client.do_action_with_exception(request)
     responseStr = str(response, encoding = "utf8")
@@ -25,3 +35,83 @@ def GetAliyunClient(configPath):
     domainName = UserConfig['domainName']
     client = AcsClient(accessKeyId, accessSecret, 'cn-hangzhou')
     return client
+
+#获取当前公网地址
+def GetPublicIpAddress():
+    requestIpUrl="http://ip.42.pl/raw"
+    response=urllib.request.urlopen(requestIpUrl).read()
+    responseIpAddress = str(response, encoding = "utf8")
+    return responseIpAddress
+
+#获取指定域名的所有子域名信息
+#https://help.aliyun.com/document_detail/29776.html?spm=a2c4g.11186623.6.650.94a13b59Q6kBCd
+def DescribeDomainRecordsRequestHelper(Client, DomainName):
+    Request = DescribeDomainRecordsRequest()
+    Request.set_accept_format('json')
+
+    Request.set_DomainName(DomainName)
+
+    result = ExecuteGetResults(Client,Request)
+    print(result)
+    return result
+
+#增加主机记录
+#https://help.aliyun.com/document_detail/29772.html?spm=a2c4g.11186623.6.653.46333b595r89tS
+def AddDomainRecordHelper(Client, DomainName, RR, Type, Value, Line):
+    Request = AddDomainRecordRequest()
+    Request.set_accept_format('json')
+
+    #设置增加记录的信息
+    Request.set_DomainName(DomainName)
+    Request.set_RR(RR)
+    Request.set_Type(Type)
+    Request.set_Value(Value)
+    Request.set_Line(Line)
+
+    result = ExecuteGetResults(Client,Request)
+    print(result)
+    return result
+
+#根据RecordID删除记录
+#https://help.aliyun.com/document_detail/29773.html?spm=a2c4g.11186623.6.654.4f611cebnCahyS
+def DeleteDomainRecordHelper(Client, RecordId):
+    Request = DeleteDomainRecordRequest()
+    Request.set_accept_format(RecordId)
+
+    #设置待删除记录的RecordId
+    Request.set_RecordId(RecordId)
+
+    result = ExecuteGetResults(Client,Request)
+    print(result)
+    return result
+
+#根据主机记录删除记录
+#https://help.aliyun.com/document_detail/29779.html?spm=a2c4g.11186623.6.656.570d2846hOPSu6
+def DeleteSubDomainRecordsHelper(Client, DomainName, RR):
+    Request = DeleteSubDomainRecordsRequest()
+    Request.set_accept_format('json')
+
+    #设置待删除记录的信息
+    Request.set_DomainName(DomainName)
+    Request.set_RR(RR)
+
+    result = ExecuteGetResults(Client,Request)
+    print(result)
+    return result
+
+#根据修改RecordId对应的记录的信息
+#https://help.aliyun.com/document_detail/29774.html?spm=a2c4g.11186623.6.655.43fd3192UJmbmD
+def UpdateDomainRecordHelper(Client, RecordId, RR, Type, Value, Line):
+    Request = UpdateDomainRecordRequest()
+    Request.set_accept_format('json')
+
+    #设置新的记录的信息
+    Request.set_RecordId(RecordId)
+    Request.set_RR(RR)
+    Request.set_Type(Type)
+    Request.set_Value(Value)
+    Request.set_Line(Line)
+
+    result = ExecuteGetResults(Client,Request)
+    print(result)
+    return result
---
+++ test.py
@@ -0,0 +1,17 @@
+import json
+import os
+import sys
+from commonModule import *
+
+userConfigPath = "C:/Users/windr/Documents/userInfo.json"
+UserConfig = ReadUserConfig(userConfigPath)
+
+domainName = UserConfig['domainName']
+
+client = GetAliyunClient(userConfigPath)
+
+DescribeDomainRecordsRequestHelper(client,domainName)
+#AddDomainRecordHelper(client, domainName, "qwe", "A", "1.2.3.4", "default")
+#UpdateDomainRecordHelper(client, "19051380032864256", "bbb", "A", "8.8.8.8", "default")
+#DescribeDomainRecordsRequestHelper(client,domainName)
+
